137a0056574712638e7a01e1c8fc4c2d48d3c582
Maxime <maxime.b2494@gmail.com>
2024-08-09 09:30:29 +0200

    création du projet

---
+++ src/main/java/fr/doranco/nomad_odyssey/NomadOdysseyApplication.java
@@ -0,0 +1,13 @@
+package fr.doranco.nomad_odyssey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomadOdysseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomadOdysseyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/doranco/nomad_odyssey/NomadOdysseyApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.doranco.nomad_odyssey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadOdysseyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
